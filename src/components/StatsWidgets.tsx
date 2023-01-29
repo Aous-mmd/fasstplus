f69e0f56693d8d783aac8e659dc9efa2e66967b4
Aous Mohammad <aous.mohammad97@gmail.com>
2023-01-29 11:48:38 +0300

    add titles

--- src/components/StatsWidgets.tsx
+++ src/components/StatsWidgets.tsx
@@ -7,7 +7,7 @@ import {
 import { CChartLine, CChartBar } from '@coreui/react-chartjs';
 import { useTranslation } from 'react-i18next';
 import { TStats } from '../pages/Stats/types';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 
 type Props = {
     data: TStats;
@@ -23,65 +23,6 @@ const StatsWidgets = (props: Props) => {
                     color="primary"
                     value={props.data?.users_count}
                     title={t('Users')}
-                    chart={
-                        <CChartLine
-                            className="mt-3 mx-3"
-                            style={{ height: '70px' }}
-                            data={{
-                                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                                datasets: [
-                                    {
-                                        label: 'My First dataset',
-                                        backgroundColor: 'transparent',
-                                        borderColor: 'rgba(255,255,255,.55)',
-                                        pointBackgroundColor: '#321fdb',
-                                        data: [65, 59, 84, 84, 51, 55, 40],
-                                    },
-                                ],
-                            }}
-                            options={{
-                                plugins: {
-                                    legend: {
-                                        display: false,
-                                    },
-                                },
-                                maintainAspectRatio: false,
-                                scales: {
-                                    x: {
-                                        grid: {
-                                            display: false,
-                                            drawBorder: false,
-                                        },
-                                        ticks: {
-                                            display: false,
-                                        },
-                                    },
-                                    y: {
-                                        min: 30,
-                                        max: 89,
-                                        display: false,
-                                        grid: {
-                                            display: false,
-                                        },
-                                        ticks: {
-                                            display: false,
-                                        },
-                                    },
-                                },
-                                elements: {
-                                    line: {
-                                        borderWidth: 1,
-                                        tension: 0.4,
-                                    },
-                                    point: {
-                                        radius: 4,
-                                        hitRadius: 10,
-                                        hoverRadius: 4,
-                                    },
-                                },
-                            }}
-                        />
-                    }
                 />
             </CCol>
             <CCol sm={3}>
@@ -90,64 +31,6 @@ const StatsWidgets = (props: Props) => {
                     color="info"
                     value={props.data?.providers_count}
                     title={t('providers_count')}
-                    chart={
-                        <CChartLine
-                            className="mt-3 mx-3"
-                            style={{ height: '70px' }}
-                            data={{
-                                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                                datasets: [
-                                    {
-                                        label: 'My First dataset',
-                                        backgroundColor: 'transparent',
-                                        borderColor: 'rgba(255,255,255,.55)',
-                                        pointBackgroundColor: '#39f',
-                                        data: [1, 18, 9, 17, 34, 22, 11],
-                                    },
-                                ],
-                            }}
-                            options={{
-                                plugins: {
-                                    legend: {
-                                        display: false,
-                                    },
-                                },
-                                maintainAspectRatio: false,
-                                scales: {
-                                    x: {
-                                        grid: {
-                                            display: false,
-                                            drawBorder: false,
-                                        },
-                                        ticks: {
-                                            display: false,
-                                        },
-                                    },
-                                    y: {
-                                        min: -9,
-                                        max: 39,
-                                        display: false,
-                                        grid: {
-                                            display: false,
-                                        },
-                                        ticks: {
-                                            display: false,
-                                        },
-                                    },
-                                },
-                                elements: {
-                                    line: {
-                                        borderWidth: 1,
-                                    },
-                                    point: {
-                                        radius: 4,
-                                        hitRadius: 10,
-                                        hoverRadius: 4,
-                                    },
-                                },
-                            }}
-                        />
-                    }
                 />
             </CCol>
             <CCol sm={3}>
@@ -156,51 +39,6 @@ const StatsWidgets = (props: Props) => {
                     color="warning"
                     value={props.data?.today_orders}
                     title={t('today_orders')}
-                    chart={
-                        <CChartLine
-                            className="mt-3"
-                            style={{ height: '70px' }}
-                            data={{
-                                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                                datasets: [
-                                    {
-                                        label: 'My First dataset',
-                                        backgroundColor: 'rgba(255,255,255,.2)',
-                                        borderColor: 'rgba(255,255,255,.55)',
-                                        data: [78, 81, 80, 45, 34, 12, 40],
-                                        fill: true,
-                                    },
-                                ],
-                            }}
-                            options={{
-                                plugins: {
-                                    legend: {
-                                        display: false,
-                                    },
-                                },
-                                maintainAspectRatio: false,
-                                scales: {
-                                    x: {
-                                        display: false,
-                                    },
-                                    y: {
-                                        display: false,
-                                    },
-                                },
-                                elements: {
-                                    line: {
-                                        borderWidth: 2,
-                                        tension: 0.4,
-                                    },
-                                    point: {
-                                        radius: 0,
-                                        hitRadius: 10,
-                                        hoverRadius: 4,
-                                    },
-                                },
-                            }}
-                        />
-                    }
                 />
             </CCol>
             <CCol sm={3}>
@@ -209,73 +47,10 @@ const StatsWidgets = (props: Props) => {
                     color="danger"
                     value={props.data?.today_income}
                     title={t('today_income')}
-                    chart={
-                        <CChartBar
-                            className="mt-3 mx-3"
-                            style={{ height: '70px' }}
-                            data={{
-                                labels: [
-                                    'January',
-                                    'February',
-                                    'March',
-                                    'April',
-                                    'May',
-                                    'June',
-                                    'July',
-                                    'August',
-                                    'September',
-                                    'October',
-                                    'November',
-                                    'December',
-                                    'January',
-                                    'February',
-                                    'March',
-                                    'April',
-                                ],
-                                datasets: [
-                                    {
-                                        label: 'My First dataset',
-                                        backgroundColor: 'rgba(255,255,255,.2)',
-                                        borderColor: 'rgba(255,255,255,.55)',
-                                        data: [78, 81, 80, 45, 34, 12, 40, 85, 65, 23, 12, 98, 34, 84, 67, 82],
-                                        barPercentage: 0.6,
-                                    },
-                                ],
-                            }}
-                            options={{
-                                maintainAspectRatio: false,
-                                plugins: {
-                                    legend: {
-                                        display: false,
-                                    },
-                                },
-                                scales: {
-                                    x: {
-                                        grid: {
-                                            display: false,
-                                            drawTicks: false,
-                                        },
-                                        ticks: {
-                                            display: false,
-                                        },
-                                    },
-                                    y: {
-                                        grid: {
-                                            display: false,
-                                            drawBorder: false,
-                                            drawTicks: false,
-                                        },
-                                        ticks: {
-                                            display: false,
-                                        },
-                                    },
-                                },
-                            }}
-                        />
-                    }
                 />
             </CCol>
             <CCol sm={12}>
+                <Typography>{t('most_orders_users')}</Typography>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
@@ -307,6 +82,7 @@ const StatsWidgets = (props: Props) => {
                 </TableContainer>
             </CCol>
             <CCol sm={12} style={{ marginTop: 5 }}>
+                <Typography>{t('most_orders_providers')}</Typography>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
@@ -338,6 +114,7 @@ const StatsWidgets = (props: Props) => {
                 </TableContainer>
             </CCol>
             <CCol sm={12} style={{ marginTop: 5 }}>
+                <Typography>{t('top_time_service')}</Typography>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
@@ -360,6 +137,7 @@ const StatsWidgets = (props: Props) => {
                 </TableContainer>
             </CCol>
             <CCol sm={12} style={{ marginTop: 5 }}>
+                <Typography>{t('top_rated_service')}</Typography>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
                         <TableHead>
